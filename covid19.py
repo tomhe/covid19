0271f205147cef68071c4da023db6fa0cdb06b77
Tomas Hellberg <tomas@hellberg.name>
2020-04-01 15:19:05 +0200

    Format with black

--- covid19.py
+++ covid19.py
@@ -13,13 +13,14 @@ This notebook uses the data provided by [Johns Hopkins CSSE](https://github.com/
 
 import pandas as pd
 import numpy as np
-import requests
 import altair as alt
 
 """## Get Data from GitHub"""
 
-url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/" \
-      "csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+url = (
+    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/"
+    "csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+)
 
 df = pd.read_csv(url)
 df.sample(5)
@@ -40,7 +41,20 @@ df.sample(5)
 
 """Filter the data set on a small set of countries."""
 
-countries = ["Sweden", "Norway", "Denmark", "Finland", "Italy", "Spain", "Germany", "France", "US", "United Kingdom", "Belgium", "Netherlands"]
+countries = [
+    "Sweden",
+    "Norway",
+    "Denmark",
+    "Finland",
+    "Italy",
+    "Spain",
+    "Germany",
+    "France",
+    "US",
+    "United Kingdom",
+    "Belgium",
+    "Netherlands",
+]
 df = df[df["Country"].isin(countries)]
 df
 
@@ -63,37 +77,43 @@ ten_days
 
 """Add "Day" column, that holds "days since 10th death", to dataframe."""
 
+
 def add_days_since_10(x):
     country = x.Country
     ten_days_date = ten_days[ten_days.Country == country].Date
     return np.int((x.Date - ten_days_date).dt.days)
 
-df["Day"]= df.apply(add_days_since_10 , axis=1)
+
+df["Day"] = df.apply(add_days_since_10, axis=1)
 df
 
 """## Plot the Dataset"""
+
 
 def plot_chart(field, x_title):
     domain = (10, int(df.Deaths.max()))
 
     selection = alt.selection_multi(fields=["Country"], bind="legend")
 
-    chart = alt.Chart(df).transform_filter(
-        alt.datum.Deaths >= 10
-    ).mark_line(point=True, interpolate="monotone").encode(
-        alt.X(f"{field}:{field == 'Date' and 'T' or 'Q'}", axis=alt.Axis(title=x_title)),
-        alt.Y("Deaths:Q", scale=alt.Scale(type="log", domain=domain)),
-        alt.Color("Country:N"),
-        shape=alt.Shape("Country"),
-        tooltip=["Country", "Deaths", field],
-        opacity=alt.condition(selection, alt.value(1), alt.value(0.12))
-    ).add_selection(
-        selection
-    ).interactive().configure_point(
-        size=95
-    ).properties(
-        width="container",
-        height=700
+    chart = (
+        alt.Chart(df)
+        .transform_filter(alt.datum.Deaths >= 10)
+        .mark_line(point=True, interpolate="monotone")
+        .encode(
+            alt.X(
+                f"{field}:{field == 'Date' and 'T' or 'Q'}",
+                axis=alt.Axis(title=x_title),
+            ),
+            alt.Y("Deaths:Q", scale=alt.Scale(type="log", domain=domain)),
+            alt.Color("Country:N"),
+            shape=alt.Shape("Country"),
+            tooltip=["Country", "Deaths", field],
+            opacity=alt.condition(selection, alt.value(1), alt.value(0.12)),
+        )
+        .add_selection(selection)
+        .interactive()
+        .configure_point(size=95)
+        .properties(width="container", height=700)
     )
 
     return chart
@@ -125,10 +145,12 @@ chart1 = plot_chart("Date", "Date")
 chart2 = plot_chart("Day", "Number of days since ~10th death")
 
 with open("docs/index.html", "w") as f:
-    f.write(two_charts_template.format(
-        vega_version=alt.VEGA_VERSION,
-        vegalite_version=alt.VEGALITE_VERSION,
-        vegaembed_version=alt.VEGAEMBED_VERSION,
-        spec1=chart1.to_json(indent=None),
-        spec2=chart2.to_json(indent=None),
-    ))
+    f.write(
+        two_charts_template.format(
+            vega_version=alt.VEGA_VERSION,
+            vegalite_version=alt.VEGALITE_VERSION,
+            vegaembed_version=alt.VEGAEMBED_VERSION,
+            spec1=chart1.to_json(indent=None),
+            spec2=chart2.to_json(indent=None),
+        )
+    )
